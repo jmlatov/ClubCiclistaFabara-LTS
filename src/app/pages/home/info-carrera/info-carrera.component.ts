5f1a07e2339bdbc569cb60f1c42335d556bdf4bf
JoséMi <josemiguel.latorre@inycom.es>
2026-02-26 15:44:36 +0100

    c.e

--- src/app/pages/home/info-carrera/info-carrera.component.ts
+++ src/app/pages/home/info-carrera/info-carrera.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { PrimaryBtnComponent } from '../../../shared/ui/buttons/primary-btn/primary-btn.component';
-import { SecondaryBtnComponent } from '../../../shared/ui/buttons/secondary-btn/secondary-btn.component';
+// import { SecondaryBtnComponent } from '../../../shared/ui/buttons/secondary-btn/secondary-btn.component';
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-info-carrera',
-    imports: [PrimaryBtnComponent, SecondaryBtnComponent],
+    imports: [PrimaryBtnComponent],
     templateUrl: './info-carrera.component.html',
     styleUrl: './info-carrera.component.css'
 })
